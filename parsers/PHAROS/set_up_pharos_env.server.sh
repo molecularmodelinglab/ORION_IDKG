0b87833f547b5e862734a350dd8cfcd3c7aa9d08
lerakaneva <lerakaneva@gmail.com>
2025-08-26 15:45:53 -0400

    Upd requirements and commit server files

---
+++ parsers/PHAROS/set_up_pharos_env.server.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+#These are the environment variables used for connecting to a PHAROS database
+export PHAROS_DB_HOST=localhost
+export PHAROS_DB_USER=ds-user
+export PHAROS_DB_PASSWORD=ds-pass
+export PHAROS_DB_NAME=pharos319
+export PHAROS_DB_PORT=3307
